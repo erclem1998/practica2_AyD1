2d508b4bc4f9987b617810b5678a46f6baa64937
Edward Gomez <2741424220101@ingenieria.usac.edu.gt>
2020-09-18 13:23:51 -0600

    Se agrego el endpoint del login

--- backend/endpoints/usuarios.js
+++ backend/endpoints/usuarios.js
@@ -1,3 +1,24 @@
 module.exports=(app,connection)=>{
-    
+    app.post('/login', (req,res) => {
+        let { username,passwd } = req.body;
+        let sql = `SELECT * FROM USERPRACTICA`;
+        connection.query(sql,function (err, rows, fields) {
+            if (!err) {
+                let log = false;
+                for(let i=0; i<rows.length; i++){
+                    if(rows[i].username == username && rows[i].passwd == passwd){
+                        log=true;
+                    }
+                }
+                if(log){
+                    res.status(200).send(true);
+                }else{
+                    res.status(404).send(false);
+                }
+            }
+            else {
+                res.status(404).send('usuario no registrado');
+            }
+        });
+    });
 }
