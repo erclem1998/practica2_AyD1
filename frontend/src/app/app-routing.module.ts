a4b4c0aa4502b2e64cc00c3d60e15940bb44fa9a
Erick Alexander Lemus Morales <erick.lemus1998@gmail.com>
2020-09-18 15:37:24 -0600

    Se agrego un matcard y una tabla para mostrar las reservaciones

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { VerReservacionesComponent } from "./components/ver-reservaciones/ver-reservaciones.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'ver_reservaciones',
+    component: VerReservacionesComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {MatCardModule} from '@angular/material/card';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -14,7 +16,8 @@ import { VerReservacionesComponent } from './components/ver-reservaciones/ver-re
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatCardModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
