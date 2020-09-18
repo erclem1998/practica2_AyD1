ad9305e801895a94edf9b00fdf01856bde34e234
Erick Alexander Lemus Morales <erick.lemus1998@gmail.com>
2020-09-18 16:54:18 -0600

    Se agregó la funcion de obtener todas las reservaciones

--- backend/endpoints/reservaciones.js
+++ backend/endpoints/reservaciones.js
@@ -1,3 +1,12 @@
 module.exports=(app,connection)=>{
-    
+    app.get('/ver_reservaciones',(req,res)=>{
+        connection.query(`select * from RESERVACION`, function (err, rows, fields) {
+            if (!err) {
+                res.status(200).send(rows)
+            }
+            else {
+                res.status(404).send(err);
+            }
+        });
+    });
 }
