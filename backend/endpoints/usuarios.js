6c95d120f4fdc00e5483f866aa3fa35b54fb516f
Henry Galvez <soulofhenry@gmail.com>
2020-09-19 03:36:16 -0600

    se añadio endpoint para eliminar usuario

--- backend/endpoints/usuarios.js
+++ backend/endpoints/usuarios.js
@@ -1,3 +1,12 @@
 module.exports=(app,connection)=>{
-    
+    app.post('/eliminar_usuario',(req,res)=>{
+        connection.query(`delete from USUARIOPRACTICA where dpi=${parseInt(req.body.dpi)}`, function (err, rows, fields) {
+            if (!err) {
+                res.status(200).send({message: "Se elimino con exito"})
+            }
+            else {
+                res.status(404).send({message: "No se pudo eliminar"});
+            }
+        });
+    });
 }
