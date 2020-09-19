22bec2ba19302de45cc095e12c713a9bb303601d
Edward Gomez <2741424220101@ingenieria.usac.edu.gt>
2020-09-18 21:51:55 -0600

    Se realizo un ajuste en endpoint de login

--- backend/endpoints/usuarios.js
+++ backend/endpoints/usuarios.js
@@ -5,15 +5,23 @@ module.exports=(app,connection)=>{
         connection.query(sql,function (err, rows, fields) {
             if (!err) {
                 let log = false;
+                let encontrado = false;
                 for(let i=0; i<rows.length; i++){
-                    if(rows[i].username == username && rows[i].passwd == passwd){
-                        log=true;
+                    if(rows[i].username == username){
+                        encontrado=true;
+                        if(rows[i].passwd == passwd){
+                            log=true;
+                        }
                     }
                 }
-                if(log){
-                    res.status(200).send(true);
+                if(encontrado){
+                    if(log){
+                        res.status(200).send('login correcto');
+                    }else{
+                        res.status(404).send('passwd incorrecto');
+                    }
                 }else{
-                    res.status(404).send(false);
+                    res.status(404).send('username no registrado');    
                 }
             }
             else {
