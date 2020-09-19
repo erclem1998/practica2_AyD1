f7c66d3dba5145ab2ef236133e9c155158f3e05f
René Corona <3114327190409@ingenieria.usac.edu.gt>
2020-09-19 01:26:32 -0600

    Se agrego las pruebas del modelo de usuario y servicio de actualizacion de usuario (Pruebas Pasadas)

--- frontend/src/app/services/update_user/update-user.service.spec.ts
+++ frontend/src/app/services/update_user/update-user.service.spec.ts
@@ -1,16 +1,26 @@
 import { TestBed } from '@angular/core/testing';
 
 import { UpdateUserService } from './update-user.service';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { User } from 'src/app/models/user/user.model';
 
-describe('UpdateUserService', () => {
+
+describe('Tests Update Service', () => {
   let service: UpdateUserService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientModule],
+      providers: [UpdateUserService, HttpClient]
+    });
     service = TestBed.inject(UpdateUserService);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  it('Test funcionamiento correcto del service', (done) => {
+    service.updateuser(new User(2298821, 'uhcoronatestangular', undefined, undefined, undefined)).subscribe((res: any) => {
+      expect(res.affectedRows).toBe(1);
+      done();
+    });
   });
+
 });
--- frontend/src/app/services/update_user/update-user.service.ts
+++ frontend/src/app/services/update_user/update-user.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { User } from 'src/app/models/user/user.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UpdateUserService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  updateuser(user: User) {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json'
+      })
+    };
+    return this.http.put('http://localhost:3000/users/', user, httpOptions)
+  }
 }
