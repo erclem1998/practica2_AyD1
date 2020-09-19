48e9567916d17fd6288314c96c3dcdc2ab5a136c
Edward Gomez <2741424220101@ingenieria.usac.edu.gt>
2020-09-18 19:37:08 -0600

    Se agrego prueba para validar que username no sea nulo

--- frontend/src/app/components/login/login.component.spec.ts
+++ frontend/src/app/components/login/login.component.spec.ts
@@ -4,6 +4,7 @@ import { LoginComponent } from './login.component';
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
+  let MockDB;
   
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -18,7 +19,15 @@ describe('LoginComponent', () => {
     fixture.detectChanges();
   });
 
+  beforeEach(() => {
+    MockDB = [{username: 'uhcorona',passwd: 'uhcorona'},{username: 'erick1234',passwd: 'erick123'},{username: '',passwd: 'carlos456'},{username: 'henryg123',passwd: ''}];
+  });
+
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('verificar username no sea vacio', () => {
+    expect(component.UsernameVacio(MockDB[0].username)).toBeFalse();
   });
 });
--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -15,4 +15,7 @@ export class LoginComponent implements OnInit {
 
   ngOnInit(): void {
   }
+
+  Login(){
+  }
 }
