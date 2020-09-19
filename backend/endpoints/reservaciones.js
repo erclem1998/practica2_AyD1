1af062ab59df45499b0fe37adf87d803bbc69c04
Henry Galvez <soulofhenry@gmail.com>
2020-09-18 19:45:18 -0600

    se añadio endpoint para actualizar una reservacion

--- backend/endpoints/reservaciones.js
+++ backend/endpoints/reservaciones.js
@@ -1,3 +1,22 @@
-module.exports=(app,connection)=>{
-    
+module.exports = (app, connection) => {
+    app.post('/actualizar_reservacion', (req, res) => {
+        var {
+            id_reservacion,
+            num_habitaciones,
+            fecha_entrada,
+            fecha_salida,
+            num_adultos,
+            num_ninios,
+            total,
+            dpi,
+        } = req.body
+        connection.query(`update RESERVACION set num_habitaciones=${num_habitaciones}, fecha_entrada=STR_TO_DATE(REPLACE('${fecha_entrada}','/','.') ,GET_FORMAT(date,'EUR')), fecha_salida=STR_TO_DATE(REPLACE('${fecha_salida}','/','.') ,GET_FORMAT(date,'EUR')),num_adultos=${num_adultos},num_ninios=${num_ninios},total=${total}, dpi=${dpi} where id_reservacion=${id_reservacion}`, function (err, rows, fields) {
+            if (!err) {
+                res.status(200).send({message: "Se actualizo correctamente la reservacion No. " + id_reservacion})
+            }
+            else {
+                res.status(404).send({message: "Error al actualizar la reservacion No. " + id_reservacion})
+            }
+        });
+    });
 }
