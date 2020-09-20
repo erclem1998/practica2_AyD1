970bb8f63435d155e8bade8600749346724b09c8
Edward Gomez <2741424220101@ingenieria.usac.edu.gt>
2020-09-19 20:35:31 -0600

    Se agrego prueba para verificar el registro

--- frontend/src/app/services/crear_usuario/crear-user.service.spec.ts
+++ frontend/src/app/services/crear_usuario/crear-user.service.spec.ts
@@ -4,6 +4,20 @@ import { CrearUserService } from './crear-user.service';
 
 describe('CrearUserService', () => {
   let service: CrearUserService;
+  let MockDBUser;
+  let mockregistro = jasmine.createSpy().and.callFake(function(user,dbmock){
+    let encontrado = false;
+    for(let i=0;i<dbmock.length;i++){
+      if(dbmock[i].dpi == user.dpi){
+        encontrado = true;
+      }
+    }
+    if(!encontrado){
+      return 'usuario registrado con exito';
+    }else{
+      return 'usuario ya registrado';
+    }
+  });
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -15,5 +29,20 @@ describe('CrearUserService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('verificar registro exitoso', (done) => {
+    service.getMockUsers().subscribe((ress: any[]) => {
+      MockDBUser = ress;
+      let user = {
+        dpi: MockDBUser[4].dpi+5,
+        username: MockDBUser[4].username+'123',
+        passwd: MockDBUser[4].passwd,
+        correo: MockDBUser[4].correo,
+        edad: MockDBUser[4].edad
+      }
+      expect(mockregistro(user,MockDBUser)).toBe('usuario registrado con exito');
+      done();
+    }); 
   });
 });
